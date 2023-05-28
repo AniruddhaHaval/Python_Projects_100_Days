94f84b1a6f60d3f0298b65c9603f89ad3a013bec
Aniruddha Haval <havalaniruddha@gmail.com>
2023-05-28 08:10:10 +0530

    complete prettytable of Pokemon Type.

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-# import another_module
-# print(another_module.another_variable)
+# # import another_module
+# # print(another_module.another_variable)
+#
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-from turtle import Turtle, Screen
+from prettytable import PrettyTable
+table = PrettyTable()
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("coral")
-timmy.forward(100)
-
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+print(table.align)
+table.align = "l"
+print(table)
